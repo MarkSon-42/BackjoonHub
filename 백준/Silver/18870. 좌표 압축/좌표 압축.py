b5065f36f07f6d9d6af773dee294db79a1f4c12f
Minwoo Son <mark42son@gmail.com>
2023-11-29 15:15:15 +0900

    [Silver II] Title: 좌표 압축, Time: 1916 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,24 +1,23 @@
-# 복잡도를 신경써야 한다
-
-# 탐색의 범위를 좀 줄여야 할것
-
-# 즉, 작은 값부터 시작해서 0부터 인덱스를 부여하는 방식
-
 import sys
 
 input = sys.stdin.readline
+
 n = int(input())
 
 num_list = list(map(int, input().split()))
+# [1000, 999, 1000, 999]
 
-temp = list(set(num_list))
+temp = list(set(num_list))  # [1000, 999]
 
-temp.sort()
+temp.sort()  # [999, 1000]
 
-number_dict = dict()
+number_dict = dict()  # {}
+
+# 아래 라인부터는 특정 테스트케이스에 대해서 동작을 적어보던지 해야함 _ debugger로는 충분치 않음.
 
 for i in range(len(temp)):
     number_dict[temp[i]] = i
+
 
 for j in num_list:
     print(number_dict[j], end=" ")
