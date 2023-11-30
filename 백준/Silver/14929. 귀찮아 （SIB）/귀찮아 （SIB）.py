922728bb92434be71d836cd4246e281fef204994
Minwoo Son <mark42son@gmail.com>
2023-12-01 08:49:27 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 100 ms, Memory: 42204 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+x = list(map(int, input().split()))
+
+num = [x[0]]
+
+for i in range(1, n):
+    num.append(num[i-1] + x[i])
+    
+answer = 0 
+
+for i in range(n):
+    answer += x[i] * (num[n - 1] - num[i])
+    
+print(answer)
