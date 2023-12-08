efb1669082ba501a03da77377e59404b8a78fe0b
Minwoo Son <mark42son@gmail.com>
2023-12-08 19:35:09 +0900

    [level 3] Title: 아이템 줍기, Time: 0.49 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -0,0 +1,56 @@
+# 문제가 참신하네
+# 핵심은 테두리만 도는거야
+# 테두리를 어떻게 나타내지..?
+
+# ... 
+
+from collections import deque
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    for rec in rectangle:
+        rec[0] *=2
+        rec[1] *=2
+        rec[2] *=2
+        rec[3] *=2
+    characterX *=2
+    characterY *=2
+    itemX *= 2
+    itemY *= 2
+    visited = [[0]*101 for _ in range(101)]
+    answer = 202
+    # U D R L
+    moves = [(0,1),(0,-1),(1,0),(-1,0)]
+    q = deque([(characterX,characterY,0)])
+    visited[characterX][characterY] = 1
+    checking_rec = [0 for _ in range(len(rectangle))]
+
+    while q:
+        x, y, cnt = q.popleft()
+        if (x,y) == (itemX,itemY) and answer > cnt:
+            answer = cnt
+        for move in moves:
+            dx, dy = move
+            next_x = x + dx
+            next_y = y + dy
+            if not (1<=next_x<=100 and 1<=next_y<=100):
+                # 좌표 범위를 벗어난다면
+                continue
+            if visited[next_x][next_y] == 1:
+                # 방문한 적이 있다면
+                continue
+            for i,rec in enumerate(rectangle):
+                s_x, s_y, e_x, e_y = rec
+                # 좌표가 직사각형 바깥으로 나갔다면
+                if (next_x < s_x or next_x > e_x) or (next_y < s_y or next_y > e_y):
+                    # 일단 여기선 문제가 안되는데, 모두 다 안되면 X
+                    checking_rec[i] = -1
+                    continue
+                # 좌표가 직사각형 중앙에 있다면
+                if (s_x < next_x < e_x) and (s_y < next_y < e_y):
+                    checking_rec[i] = -2
+                    break
+                checking_rec[i] = 1
+            if -2 in checking_rec or 1 not in checking_rec:
+                continue
+            visited[next_x][next_y] = 1
+            q.append((next_x,next_y,cnt+1))
+    return answer//2
