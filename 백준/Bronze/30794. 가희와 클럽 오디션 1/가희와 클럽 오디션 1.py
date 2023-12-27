1117a4d491216d2c61909640bd52d470d3325de1
Minwoo Son <mark42son@gmail.com>
2023-12-27 13:02:23 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.py
@@ -0,0 +1,13 @@
+lv, result = input().split()
+lv = int(lv)
+
+if result == "miss":
+    print(lv * 0)
+if result == "bad":
+    print(lv * 200)
+if result == "cool":
+    print(lv * 400)
+if result == "great":
+    print(lv * 600)
+if result == "perfect":
+    print(lv * 1000)
