1316601ed666a9591b7230ba4f01e5b1f21063a5
Minwoo Son <mark42son@gmail.com>
2024-01-09 17:33:55 +0900

    [level 1] Title: 공원 산책, Time: 0.11 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,30 +1,30 @@
 def solution(park, routes):
-    x, y = 0, 0  # 시작점
-    w, h = len(park[0]), len(park)  # 가로, 세로
-    op = {"N": (-1, 0), "S": (1, 0), "W": (0, -1), "E": (0, 1)}
-
-    # 시작점
+    x, y = 0, 0  # 시작점 초기화
+    w, h = len(park[0]), len(park)
+    
+    op = {"N": (-1, 0),"S": (1, 0),"W": (0, -1),"E": (0, 1)}
+    
+    # 시작점 S찾기
+    
     for i in range(h):
         for j in range(w):
-            if park[i][j] == "S":
+            if park[i][j] == 'S':
                 x, y = i, j
                 break
-
-    # 좌표이동
+                
+    # routes 명렁어 처리
     for r in routes:
-        d, n = r.split(" ")  # 방향, 이동횟수
-        dx, dy = x, y  # 현재위치
-
+        d, n = r.split(" ")
+        dx, dy = x, y
+        
         for i in range(int(n)):
-            # 이동할 위치
-            nx = x + op[d][0]
+            nx = x + op[d][0]   # d : Key  'N', 'S',..
             ny = y + op[d][1]
-
-            if 0 <= nx <= h - 1 and 0 <= ny <= w - 1 and (park[nx][ny] != "X"):
+            
+            if 0 <= nx <= h - 1 and 0 <= ny <= w - 1 and (park[nx][ny] != 'X'):
                 x, y = nx, ny
-
             else:
                 x, y = dx, dy
                 break
-
-    return (x, y)
+                
+    return x, y
