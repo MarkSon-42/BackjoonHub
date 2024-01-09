4a479b7d7fb7ba12dad7da5502c8ac69363eac41
Minwoo Son <mark42son@gmail.com>
2024-01-09 13:55:21 +0900

    [level 1] Title: 덧칠하기, Time: 183.44 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,11 +1,12 @@
-# 조건문과 반복문의 기초적인 활용을 묻는 문제라고 생각.
+# 롤러가 벽에서 벗어나면 안 됩니다.
+# 구역의 일부분만 포함되도록 칠하면 안 됩니다.
 
 def solution(n, m, section):
     answer = 0
-    while section:
-        left = section[0]
-        right = left + m
+    while section:  
+        left = section[0]  # 6
+        right = section[0] + m  # 10
         while section and section[0] < right:
             del section[0]
         answer += 1
-    return answer
+    return answer  # 2
