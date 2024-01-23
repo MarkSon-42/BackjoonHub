a3df8218a7dcb6992ac9065136f3242e7d97a456
Minwoo Son <mark42son@gmail.com>
2024-01-23 17:10:31 +0900

    [level 2] Title: 숫자 변환하기, Time: 79.68 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,17 +1,15 @@
-from collections import deque
-
 def solution(x, y, n):
     MAX_INT = 10 ** 6
-    dist = [-1] * (MAX_INT + 1)
-    dist[x] = 0
+    dp = [0] + [MAX_INT] * MAX_INT
+    dp[x] = 0
 
-    q = deque([x])
-    while q:
-        curr = q.popleft()
-        if curr == y:
-            return dist[curr]
-        for next in (curr + n, curr * 2, curr * 3):
-            if 0 <= next <= MAX_INT and dist[next] == -1:
-                dist[next] = dist[curr] + 1
-                q.append(next)
-    return -1
+    for i in range(x, y + 1):
+        if dp[i] != MAX_INT:
+            if i + n <= y:
+                dp[i + n] = min(dp[i + n], dp[i] + 1)
+            if i * 2 <= y:
+                dp[i * 2] = min(dp[i * 2], dp[i] + 1)
+            if i * 3 <= y:
+                dp[i * 3] = min(dp[i * 3], dp[i] + 1)
+
+    return dp[y] if dp[y] != MAX_INT else -1
