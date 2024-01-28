de420b7b992b9f9ca272bf59b61bcd35246645bb
Minwoo Son <mark42son@gmail.com>
2024-01-28 16:39:42 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -2,24 +2,23 @@ N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 room = [list(map(int, input().split())) for _ in range(N)]
 
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]  # North, East, South, West
-cleaned = [[0]*M for _ in range(N)]
-cleaned[r][c] = 1  # Start cell is cleaned
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]  
+room[r][c] = 2  
 count = 1
 
 while True:
     check = False
     for _ in range(4):
-        d = (d+3) % 4  # Rotate to left
+        d = (d+3) % 4
         nx, ny = r + dx[d], c + dy[d]
-        if room[nx][ny] == 0 and cleaned[nx][ny] == 0:  # If the cell is not cleaned
+        if room[nx][ny] == 0:
             r, c = nx, ny
-            cleaned[r][c] = 1
+            room[r][c] = 2
             count += 1
             check = True
             break
-    if not check:  # If all cells around are cleaned or wall
-        if room[r-dx[d]][c-dy[d]] == 1:  # If the backward cell is wall
+    if not check: 
+        if room[r-dx[d]][c-dy[d]] == 1: 
             break
         else:
             r, c = r - dx[d], c - dy[d]
