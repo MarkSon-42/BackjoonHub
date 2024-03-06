a61b737a0c2c8d75742b8275149d3a1f2dca2b45
Minwoo Son <mark42son@gmail.com>
2024-03-06 17:53:16 +0900

    [Gold IV] Title: 불!, Time: 2356 ms, Memory: 39484 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = []
+
+for i in range(n):
+    graph.append(list(input().rstrip()))
+    if 'J' in graph[i]:
+        queue = deque([(0, i, graph[i].index('J'))])
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 'F':
+            queue.append((-1, i, j))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+answer = 'IMPOSSIBLE'
+
+while queue:
+    time, x, y = queue.popleft()
+    
+    if time > -1 and graph[x][y] != 'F' and (x == 0 or y == 0 or x == n - 1 or y == m - 1):
+        answer = time + 1
+        break
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != '#':
+
+            if time > -1 and graph[nx][ny] == '.':
+                graph[nx][ny] = '_'
+                queue.append((time + 1, nx, ny))
+
+            elif time == -1 and graph[nx][ny] != 'F':
+                graph[nx][ny] = 'F'
+                queue.append((-1, nx, ny))
+
+print(answer)
