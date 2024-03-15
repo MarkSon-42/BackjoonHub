620874ad1d6045155e26628d0bcb38695c891ebf
Minwoo Son <mark42son@gmail.com>
2024-03-16 08:30:48 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -2,13 +2,11 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    max_drink = 0
-    max_school = ""
+    schools = set()
     for i in range(n):
         school_name, drink = map(str, input().split())
         drink = int(drink)
-        if drink > max_drink:
-            max_drink = drink
-            max_school = school_name
-    print(max_school)
+        schools.add((drink, school_name))
+        
+    print(max(schools)[1])
         
