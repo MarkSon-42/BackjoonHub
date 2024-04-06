1a32cbead2dc0c3242243b51de229107bb16a0d2
Minwoo Son <mark42son@gmail.com>
2024-04-06 15:08:26 +0900

    [Silver II] Title: 침투, Time: 456 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.py
+++ 백준/Silver/13565. 침투/침투.py
@@ -1,29 +1,38 @@
 from collections import deque
 import sys
-input=sys.stdin.readline
 
-dr=[(1,0), (0,1), (-1,0), (0,-1)]
+# Define directions
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-N,M=map(int,input().split())
-MAP=[list(map(int,input().rstrip())) for _ in range(N)]
+# Read input
+N, M = map(int, sys.stdin.readline().split())
+grid = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
 
-def bfs(x,y):
-    dq=deque()
-    dq.append([x,y])
-    while dq:
-        X,Y=dq.popleft()
-        if X==N-1:
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        # If we reached the bottom row
+        if x == N - 1:
             print('YES')
             exit(0)
-        for a,b in dr:
-            mx,my=X+a,Y+b
-            if 0<=mx<N and 0<=my<M:
-                if MAP[mx][my]==0:
-                    MAP[mx][my]=1
-                    dq.append([mx,my])
+
+        # Check all directions
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+
+            # If the new position is within the grid and is a white cell
+            if 0 <= nx < N and 0 <= ny < M and grid[nx][ny] == 0:
+                # Mark the cell as visited
+                grid[nx][ny] = 1
+                # Add the new position to the queue
+                queue.append((nx, ny))
 
 for i in range(M):
-    if MAP[0][i]==0:
-        bfs(0,i)
+    if grid[0][i] == 0:
+        bfs(0, i)
 
 print('NO')
