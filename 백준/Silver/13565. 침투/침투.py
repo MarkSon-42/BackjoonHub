18fb4d6438a676338bd940da6875ee79fda46f64
Minwoo Son <mark42son@gmail.com>
2024-04-06 15:01:45 +0900

    [Silver II] Title: 침투, Time: 464 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.py
+++ 백준/Silver/13565. 침투/침투.py
@@ -1,31 +1,29 @@
+from collections import deque
 import sys
+input=sys.stdin.readline
 
-sys.setrecursionlimit(10 ** 6)
-n, m = map(int, sys.stdin.readline().split())
-array = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
-visted = [[False] * m for _ in range(n)]
+dr=[(1,0), (0,1), (-1,0), (0,-1)]
 
-d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+N,M=map(int,input().split())
+MAP=[list(map(int,input().rstrip())) for _ in range(N)]
 
+def bfs(x,y):
+    dq=deque()
+    dq.append([x,y])
+    while dq:
+        X,Y=dq.popleft()
+        if X==N-1:
+            print('YES')
+            exit(0)
+        for a,b in dr:
+            mx,my=X+a,Y+b
+            if 0<=mx<N and 0<=my<M:
+                if MAP[mx][my]==0:
+                    MAP[mx][my]=1
+                    dq.append([mx,my])
 
-def dfs(r, c):
-    global flag
-    if r >= n:
-        flag = True
-    if 0 <= r < n and 0 <= c < m and not visted[r][c] and array[r][c] == 0:
-        visted[r][c] = True
-        for drc in d:
-            dr = r + drc[0]
-            dc = c + drc[1]
-            dfs(dr, dc)
+for i in range(M):
+    if MAP[0][i]==0:
+        bfs(0,i)
 
-
-flag = False
-for c in range(m):
-    if array[0][c] == 0:
-        dfs(0, c)
-
-if flag:
-    print('YES')
-else:
-    print('NO')
+print('NO')
