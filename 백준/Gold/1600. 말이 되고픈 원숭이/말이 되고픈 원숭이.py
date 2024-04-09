33a4a4572e9f26e73798b10ecc90d61567849977
Minwoo Son <mark42son@gmail.com>
2024-04-10 08:37:09 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 3076 ms, Memory: 51796 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,3 +1,4 @@
+
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -12,29 +13,29 @@ def bfs(n):
     q.append((0, 0, n))
     count = [[[0] * (n + 1) for _ in range(w)] for _ in range(h)]
     while q:
-        x, y, K = q.popleft()
+        x, y, k = q.popleft()
         if x == h-1 and y == w-1:
-            return count[x][y][K]
-        if K > 0:
-            for k in range(8):
-                nx = x + horse_dx[k]
-                ny = y + horse_dy[k]
+            return count[x][y][k]
+        if k > 0:
+            for i in range(8):
+                nx = x + horse_dx[i]
+                ny = y + horse_dy[i]
                 if 0 <= nx < h and 0 <= ny < w:
-                    if grid[nx][ny] != 1 and count[nx][ny][K-1] == 0:
-                        count[nx][ny][K-1] = count[x][y][K] + 1
-                        q.append((nx, ny, K-1))
-        for k in range(4):
-            nx = x + monkey_dx[k]
-            ny = y + monkey_dy[k]
+                    if grid[nx][ny] != 1 and count[nx][ny][k-1] == 0:
+                        count[nx][ny][k-1] = count[x][y][k] + 1
+                        q.append((nx, ny, k-1))
+        for i in range(4):
+            nx = x + monkey_dx[i]
+            ny = y + monkey_dy[i]
             if 0 <= nx < h and 0 <= ny < w:
-                if grid[nx][ny] != 1 and count[nx][ny][K] == 0:
-                    count[nx][ny][K] = count[x][y][K] + 1
-                    q.append((nx, ny, K))
+                if grid[nx][ny] != 1 and count[nx][ny][k] == 0:
+                    count[nx][ny][k] = count[x][y][k] + 1
+                    q.append((nx, ny, k))
     return -1
 
 
-K = int(input())
+k = int(input())
 w, h = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(h)]
-result = bfs(K)
+result = bfs(k)
 print(result)
