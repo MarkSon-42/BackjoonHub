7985137303914ca4736bda8d813ab18016c08eb0
Minwoo Son <mark42son@gmail.com>
2024-04-25 11:18:01 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 183 ms, Memory: 48,944 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,17 +1,9 @@
-# 쪽과 오른쪽으로 창문을 열었을 때, 양쪽 모두 거리 2 이상의 공간이 확보될 때 조망권이 확보된다
-
-#  조망권이 확보된 세대의 수를 반환하는 프로그램을 작성
-
-
-tc = 10
-
-for t in range(1, tc + 1):
-    n = int(input())
-    building = list(map(int, input().split()))
+for tc in range(1, 11):
     answer = 0
-
+    n = int(input())
+    b = list(map(int, input().split()))
     for i in range(2, n - 2):
-        if ((building[i - 2] < building[i] and building[i - 1] < building[i])
-                and (building[i + 1] < building[i] and building[i + 2] < building[i])):
-            answer += building[i] - max(building[i - 2], building[i - 1], building[i + 1], building[i + 2])
-    print(f"#{t} {answer}")
+        if max(b[i-2], b[i-1], b[i], b[i+1], b[i+2]) == b[i]:
+            answer += b[i] - max(b[i-2], b[i-1], b[i+1], b[i+2])
+
+    print(f"#{tc} {answer}")
