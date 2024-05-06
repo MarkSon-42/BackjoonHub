df81f003c39b544e31737eef48531bbc0c464bc2
Minwoo Son <mark42son@gmail.com>
2024-05-06 12:41:23 +0900

    [Silver III] Title: 퇴사, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,27 +1,30 @@
-n = int(input())
-t = [] # 상담 기간
-p = [] # 상담 비용
-max_profit = 0 # 최대 이익
 
-for i in range(n):
-    ti, pi = map(int, input().split())
-    t.append(ti)
-    p.append(pi)
-
-def backtrack(day, profit):
-    global max_profit
-    
-    # N+1번째 날짜에는 상담을 할 수 없음
-    if day == n+1:
-        max_profit = max(max_profit, profit)
+def dfs(n, sm):
+    global answer
+    # 1. 종료 조건 : 가능한 n을 종료에 관련된 것으로 정의!
+    if n >= N:
+        answer = max(answer, sm)
         return
-    
-    # 상담을 하는 경우
-    if day + t[day-1] <= n+1:
-        backtrack(day + t[day-1], profit + p[day-1])
-    
-    # 상담을 하지 않는 경우
-    backtrack(day + 1, profit)
 
-backtrack(1, 0)
-print(max_profit)
+    # 2. 하부 호출 : 화살표 개수 만큼 호출
+
+    # 2 - 1 : 상담 하는 경우
+    if n + T[n] <= N:
+        dfs(n + T[n], sm + P[n])
+
+    # 가능한 모든 경우를 해보는 것이다.
+    # 여기서 습관적으로 else를 쓰면 망한다.
+
+    # 2 - 2 : 상담하지 않는 경우
+    dfs(n + 1, sm)
+
+
+N = int(input())
+T = [0] * N
+P = [0] * N
+for i in range(N):
+    T[i], P[i] = map(int, input().split())
+
+answer = 0
+dfs(0, 0)
+print(answer)
