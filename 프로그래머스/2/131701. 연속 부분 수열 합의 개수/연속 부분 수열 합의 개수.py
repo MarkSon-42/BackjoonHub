b7b7af26429bb62cb98dc964f0a662cc2cf8bb2a
Minwoo Son <mark42son@gmail.com>
2024-05-24 13:01:12 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 327.98 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,14 +1,11 @@
 def solution(elements):
-    n = len(elements)
-    elements = elements * 2
-    numbers = set()
+    ll = len(elements)
+    res = set()
 
-    for length in range(1, n + 1):
-        window_sum = sum(elements[:length])
-        numbers.add(window_sum)
-
-        for i in range(length, n + length):
-            window_sum = window_sum - elements[i - length] + elements[i]
-            numbers.add(window_sum)
-
-    return len(numbers)
+    for i in range(ll):
+        ssum = elements[i]
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]
+            res.add(ssum)
+    return len(res)
