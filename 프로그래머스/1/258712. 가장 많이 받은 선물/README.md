5e43620b09f7d9cc88054c5a06db9301cc5cba15
Minwoo Son <mark42son@gmail.com>
2024-06-14 21:28:23 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.77 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 1.67 ms
+메모리: 10.3 MB, 시간: 1.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 12일 20:20:29
+2024년 06월 14일 21:28:18
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,30 +1,45 @@
 def solution(friends, gifts):
-    g_map, g_count, r_count, ans = {}, {}, {}, {}
 
-    for f in friends:
-        g_map[f] = {f2: 0 for f2 in friends if f != f2}
-        ans[f], g_count[f], r_count[f] = 0, 0, 0
+    gift_map = {}
+    given_map = {}
+    received_map = {}
+    answer_map = {}
+
+    for f1 in friends:
+        inner_gift_map = {}
+        for f2 in friends:
+            if f1 == f2:
+                continue
+            inner_gift_map[f2] = 0
+        gift_map[f1] = inner_gift_map
+        given_map[f1] = 0
+        received_map[f1] = 0
+        answer_map[f1] = 0
 
     for gift in gifts:
-        g, r = gift.split()
-        g_map[g][r] += 1
-        g_count[g] += 1
-        r_count[r] += 1
+        given, received = gift.split()
+        gift_map[given][received] += 1
+        given_map[given] += 1
+        received_map[received] += 1
 
     for i in range(len(friends) - 1):
         for j in range(i + 1, len(friends)):
-            A, B = friends[i], friends[j]
-            A_c, B_c = g_map[A][B], g_map[B][A]
-
-            if A_c > B_c:
-                ans[A] += 1
-            elif B_c > A_c:
-                ans[B] += 1
+            a = friends[i]
+            b = friends[j]
+            a_cnt = gift_map[a][b]            
+            b_cnt = gift_map[b][a]
+            
+            if a_cnt > b_cnt:
+                answer_map[a] += 1
+            elif a_cnt < b_cnt:
+                answer_map[b] += 1
             else:
-                A_v, B_v = g_count[A] - r_count[A], g_count[B] - r_count[B]
-                if A_v > B_v:
-                    ans[A] += 1
-                elif B_v > A_v:
-                    ans[B] += 1
-
-    return max(ans.values())
+                a_value = given_map[a] - received_map[a]
+                b_value = given_map[b] - received_map[b]
+                
+                if a_value > b_value:
+                    answer_map[a] += 1
+                elif a_value < b_value:
+                    answer_map[b] += 1
+                    
+    return max(answer_map.values())
