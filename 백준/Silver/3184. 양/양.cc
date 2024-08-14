8b14330f4328513e6dc38f204bdb0ed04c7e8539
Minwoo Son <mark42son@gmail.com>
2024-08-14 17:29:31 +0900

    [Silver I] Title: 양, Time: 4 ms, Memory: 2144 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.cc
+++ 백준/Silver/3184. 양/양.cc
@@ -1,3 +1,5 @@
+// 백준 3184 flood fill 양과 늑대 문제
+
 #include <iostream>
 #include <queue>
 
@@ -6,47 +8,51 @@ using namespace std;
 const int MAX_SIZE = 250;
 char arr[MAX_SIZE][MAX_SIZE];
 bool visited[MAX_SIZE][MAX_SIZE];
+int sheep, wolf, r, c;
 
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
+int dx[4] = { 0, 0, 1, -1 };
+int dy[4] = { 1, -1, 0, 0 };
 
-void bfs(int x, int y, int& sheepCount, int& wolfCount) {
+void bfs(int i, int j) {
     queue<pair<int, int>> q;
-    q.push({x, y});
-    visited[x][y] = true;
+    q.push({ i, j });
+    visited[i][j] = true;
+    int s = 0;
+    int w = 0;
+    if (arr[i][j] == 'o') s++;
+    if (arr[i][j] == 'v') w++;
     
-    int localSheep = 0;
-    int localWolves = 0;
-
-    // Count the initial position
-    if (arr[x][y] == 'o') localSheep++;
-    if (arr[x][y] == 'v') localWolves++;
-
     while (!q.empty()) {
-        auto [curX, curY] = q.front();
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
 
         for (int i = 0; i < 4; i++) {
-            int nx = curX + dx[i];
-            int ny = curY + dy[i];
-            if (nx >= 0 && ny >= 0 && nx < MAX_SIZE && ny < MAX_SIZE && !visited[nx][ny] && arr[nx][ny] != '#') {
-                visited[nx][ny] = true;
-                if (arr[nx][ny] == 'o') localSheep++;
-                if (arr[nx][ny] == 'v') localWolves++;
-                q.push({nx, ny});
-            }
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
+            if (visited[nx][ny] || arr[nx][ny] == '#') continue;
+            visited[nx][ny] = 1;
+            if (arr[nx][ny] == 'o') s++;
+            if (arr[nx][ny] == 'v') w++;
+            q.push({ nx, ny });
         }
     }
 
-    if (localSheep > localWolves) {
-        sheepCount += localSheep;
-    } else {
-        wolfCount += localWolves;
-    }
+    // 양이 늑대보다 많다? 오호호,, 해당 울타리 늑대 모두 아웃!
+    if (s > w) wolf -= w;
+    if (s <= w) sheep -= s;
 }
 
-int main() {
-    int r, c;
+// 글자 '.' (점)은 빈 필드를 의미하며,
+// 글자 '#'는 울타리를, 'o'는 양, 'v'는 늑대를 의미
+
+
+// 늑대일 경우 bfs호출.. 그리고 늑대를 카운트 하자
+
+
+int main()
+{
     cin >> r >> c;
 
     for (int i = 0; i < r; i++) {
@@ -54,26 +60,20 @@ int main() {
             cin >> arr[i][j];
         }
     }
-
-    int totalSheep = 0;
-    int totalWolves = 0;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (arr[i][j] == 'o') sheep++;
+            if (arr[i][j] == 'v') wolf++;
+        }
+    }
 
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
             if (arr[i][j] != '#' && !visited[i][j]) {
-                int sheepCount = 0;
-                int wolfCount = 0;
-                bfs(i, j, sheepCount, wolfCount);
-                if (sheepCount > wolfCount) {
-                    totalSheep += sheepCount;
-                } else {
-                    totalWolves += wolfCount;
-                }
+                bfs(i, j);
             }
         }
     }
 
-    cout << totalSheep << ' ' << totalWolves << endl;
-
-    return 0;
+    cout << sheep << " " << wolf;
 }
