39041e923e7d5cc43b00acc7626ba638a20a1c20
Minwoo Son <mark42son@gmail.com>
2024-08-20 20:13:32 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.cc
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.cc
@@ -4,81 +4,58 @@
 #include <algorithm>
 using namespace std;
 
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
-int N, M, T;
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+int ans_swd = 1e9;  // 검을 줍고 벽 부수며 공주를 구했을때 거리
+int ans = 1e9;  // 검 줍지 않고 길 따라 갔을때 거리
 vector<vector<int>> _map;
 vector<vector<bool>> visited;
 vector<vector<int>> dist;
 
-int bfs() {
+void bfs(int n, int m, int t) {
     queue<pair<int, int>> q;
-    q.push({0, 0});
+    q.push({ 0, 0 });
     visited[0][0] = true;
-    
-    int sword_x = -1, sword_y = -1;
-    int princess_dist = -1;
-    
     while (!q.empty()) {
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
-        
-        if (x == N-1 && y == M-1) {
-            princess_dist = dist[x][y];
-            break;
+        if (x == n - 1 && y == m - 1) {
+            ans = min(ans, dist[x][y]);
         }
-        
         if (_map[x][y] == 2) {
-            sword_x = x;
-            sword_y = y;
+            ans_swd = min(ans_swd, dist[x][y] + (n - x - 1) + (m - y - 1));
         }
-        
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (nx >= n || nx < 0 || ny >= m || ny < 0) continue;
             if (_map[nx][ny] == 1 || visited[nx][ny]) continue;
-            
             visited[nx][ny] = true;
             dist[nx][ny] = dist[x][y] + 1;
-            q.push({nx, ny});
+            q.push({ nx, ny });
         }
     }
-    
-    if (sword_x != -1) {
-        int sword_dist = dist[sword_x][sword_y];
-        int direct_to_princess = (N-1 - sword_x) + (M-1 - sword_y);
-        int with_sword = sword_dist + direct_to_princess;
-        
-        if (princess_dist == -1 || with_sword < princess_dist) {
-            princess_dist = with_sword;
-        }
-    }
-    
-    return princess_dist;
 }
 
 int main() {
-    cin >> N >> M >> T;
-    _map = vector<vector<int>>(N, vector<int>(M));
-    visited = vector<vector<bool>>(N, vector<bool>(M, false));
-    dist = vector<vector<int>>(N, vector<int>(M, 0));
-    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    int n, m, t;
+    cin >> n >> m >> t;
+    _map = vector<vector<int>>(n, vector<int>(m));
+    visited = vector<vector<bool>>(n, vector<bool>(m, false));
+    dist = vector<vector<int>>(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> _map[i][j];
         }
     }
-    
-    int result = bfs();
-    
-    if (result == -1 || result > T) {
-        cout << "Fail" << endl;
+    bfs(n, m, t);
+
+    int result = min(ans, ans_swd);
+    if (result > t || result == 1e9) {
+        cout << "Fail";
     } else {
-        cout << result << endl;
+        cout << result;
     }
-    
     return 0;
 }
