388a4fdd00e125fca95d436787ae2baf13761e61
Minwoo Son <mark42son@gmail.com>
2024-08-28 15:47:59 +0900

    [Silver I] Title: 스타트링크, Time: 8 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.cc
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -4,42 +4,41 @@
 
 using namespace std;
 
-int F, S, G, U, D;
+int building[1000005];
+vector<int> visited(1000005, -1);
+
+int f, s, g, u, d;
 
 int bfs() {
-	vector<int> visited(F + 1, -1);
 	queue<int> q;
-
-	q.push(S);
-	visited[S] = 0;
-
+	q.push(s);
+	visited[s] = 0;
 	while (!q.empty()) {
 		int curr = q.front();
+
+		if (curr == g) {  
+			cout << visited[curr];
+			return 0;
+		}
+
+		if (curr + u <= f && visited[curr + u] == -1) {
+			visited[curr + u] = visited[curr] + 1;  
+			q.push(curr + u);
+		}
+
+		if (curr - d >= 1 && visited[curr - d] == -1) {
+			visited[curr - d] = visited[curr] + 1;
+			q.push(curr - d);
+		}
 		q.pop();
-
-		if (curr == G) {
-			return visited[curr];
-		}
-
-		if (curr + U <= F && visited[curr + U] == -1) {
-			visited[curr + U] = visited[curr] + 1;
-			q.push(curr + U);
-		}
-
-		if (curr - D >= 1 && visited[curr - D] == -1) {
-			visited[curr - D] = visited[curr] + 1;
-			q.push(curr - D);
-		}
 	}
 
 	return -1;
 }
-
-
 int main() {
-	cin >> F >> S >> G >> U >> D;
-	int ans = bfs();
-	if (ans == -1) cout << "use the stairs";
-	else cout << ans;
-	return 0;
+
+	cin >> f >> s >> g >> u >> d;
+	if (bfs() == -1) {
+		cout << "use the stairs";
+	}
 }
