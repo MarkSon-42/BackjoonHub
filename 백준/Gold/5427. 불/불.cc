e37038a54c43698d83917475b0e17d4d9506647b
Minwoo Son <mark42son@gmail.com>
2024-09-03 16:13:15 +0900

    [Gold IV] Title: 불, Time: 124 ms, Memory: 8940 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,99 +1,94 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+
 using namespace std;
 
 int t, w, h;
-int dx[] = { 0, 0, 1, -1 };
-int dy[] = { 1, -1, 0, 0 };
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 char maze[1001][1001];
 int dist[1001][1001];
 
 int bfs() {
-    queue<pair<int, int>> q;
-    queue<pair<int, int>> fire;
+	// bfs적용 객체가 2개니까..큐도 2개
+	queue<pair<int, int>> q; 
+	queue<pair<int, int>> fire;
 
-    // 상근이와 불의 초기 위치를 찾아 큐에 넣습니다.
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            if (maze[i][j] == '@') {
-                q.push({ i, j });
-                dist[i][j] = 0;
-            }
-            else if (maze[i][j] == '*') {
-                fire.push({ i, j });
-            }
-            if (maze[i][j] != '#') dist[i][j] = -1;
-        }
-    }
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			if (maze[i][j] == '@') {
+				q.push({ i, j });
+				dist[i][j] = 0;
+			}
+			else if (maze[i][j] == '*') {
+				fire.push({ i, j });
+			}
+			if (maze[i][j] != '#') dist[i][j] = -1;
+		}
+	}
 
-    while (!q.empty()) {
-        // 불을 먼저 확산시킵니다.
-        int fire_size = fire.size();
-        while (fire_size--) {
-            int x = fire.front().first;
-            int y = fire.front().second;
-            fire.pop();
+	while (!q.empty()) {
+		int f_size = fire.size();  // 불 큐 크기 만큼..?
+		while (f_size--) {
+			int x = fire.front().first;
+			int y = fire.front().second;
+			fire.pop();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-                if (maze[nx][ny] == '.' || maze[nx][ny] == '@') {
-                    maze[nx][ny] = '*';
-                    fire.push({ nx, ny });
-                }
-            }
-        }
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+				if (maze[nx][ny] == '.' || maze[nx][ny] == '@') {
+					maze[nx][ny] = '*';
+					fire.push({ nx, ny });
+				}
+			}
+		}
 
-        // 상근이를 이동시킵니다.
-        int size = q.size();
-        while (size--) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
+		int q_size = q.size();
 
-            // 탈출 조건: 가장자리에 도달했을 때
-            if (x == 0 || x == h - 1 || y == 0 || y == w - 1) {
-                return dist[x][y] + 2;
-            }
+		while (q_size--) {
+			int x = q.front().first;
+			int y = q.front().second;
+			q.pop();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-                if (maze[nx][ny] == '.' && dist[nx][ny] == -1) {
-                    dist[nx][ny] = dist[x][y] + 1;
-                    q.push({ nx, ny });
-                }
-            }
-        }
-    }
-    return -1; // 탈출 불가능
+			if (x == 0 || x == h - 1 || y == 0 || y == w - 1) {
+				return dist[x][y] + 2;  // 탈출 ( 탈출 거리가 아니고 탈출했을때 초를 구하는거라 +2 해줌.
+			}
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+				if (maze[nx][ny] == '.' && dist[nx][ny] == -1) {
+					dist[nx][ny] = dist[x][y] + 1;
+					q.push({ nx, ny });
+				}
+			}
+		}
+	}
+	// 큐도 비었는데 여기까지 왔다면.. 탈출 불가능이오.
+	return -1;
 }
 
 int main() {
+	cin >> t;
+	while (t--) {
+		cin >> w >> h;
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				cin >> maze[i][j];
+				dist[i][j] = -1;
+			}
+		}
 
-//    FILE* file;
-//    freopen_s(&file, "input.txt", "r", stdin);
-
-    cin >> t;
-    while (t--) {
-        cin >> w >> h;
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                cin >> maze[i][j];
-                dist[i][j] = -1;
-            }
-        }
-
-        int result = bfs();
-        if (result == -1) {
-            cout << "IMPOSSIBLE" << endl;
-        }
-        else {
-            cout << result << endl;
-        }
-    }
-    return 0;
+		int ans = bfs();
+		if (ans == -1) {
+			cout << "IMPOSSIBLE" << endl;
+		}
+		else {
+			cout << ans << endl;
+		}
+	}
+	return 0;
 }
