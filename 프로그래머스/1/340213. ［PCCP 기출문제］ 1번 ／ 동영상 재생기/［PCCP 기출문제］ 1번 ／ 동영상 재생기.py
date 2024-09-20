4879e78e227d2f627ae412a6d0235c21336dc0a2
Minwoo Son <mark42son@gmail.com>
2024-09-20 17:46:41 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,30 +1,29 @@
+def time_to_sec(time_str):
+    minutes, seconds = map(int, time_str.split(':'))
+    return minutes * 60 + seconds
+
+def sec_to_time(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    return f"{minutes:02d}:{seconds:02d}"
+
 def solution(video_len, pos, op_start, op_end, commands):
-    def time_to_seconds(time_str):
-        minutes, seconds = map(int, time_str.split(':'))
-        return minutes * 60 + seconds
-
-    def seconds_to_time(seconds):
-        minutes, seconds = divmod(seconds, 60)
-        return f"{minutes:02d}:{seconds:02d}"
-
-    video_len_sec = time_to_seconds(video_len)
-    pos_sec = time_to_seconds(pos)
-    op_start_sec = time_to_seconds(op_start)
-    op_end_sec = time_to_seconds(op_end)
-
-    # 시작 시 오프닝 구간인 경우 먼저 건너뛰기
-    if op_start_sec <= pos_sec <= op_end_sec:
-        pos_sec = op_end_sec
-
-    for command in commands:
-        if command == "prev":
-            pos_sec = max(0, pos_sec - 10)
-        elif command == "next":
-            pos_sec = min(video_len_sec, pos_sec + 10)
+    video_len = time_to_sec(video_len)
+    curr_pos = time_to_sec(pos)
+    op_start = time_to_sec(op_start)
+    op_end = time_to_sec(op_end)
+    
+    for cmd in commands:
+        if op_start <= curr_pos <= op_end:  # 명령 실행 전 오프닝 체크
+            curr_pos = op_end
         
-        # 명령 실행 후 오프닝 구간인 경우 건너뛰기
-        if op_start_sec <= pos_sec <= op_end_sec:
-            pos_sec = op_end_sec
-
-    return seconds_to_time(pos_sec)
-
+        if cmd == "prev":
+            curr_pos = max(0, curr_pos - 10)  # 음수를 0으로 보정하는 테크닉 ㅇㅇ,,,
+            
+        elif cmd == "next":
+            curr_pos = min(video_len, curr_pos + 10)  # 비디오 최대 길이를 넘어갈시, 최대길이로 보정...
+           
+    # 명령 실행 후 한번 더 체크
+    if op_start <= curr_pos <= op_end:
+        curr_pos = op_end
+        
+    return sec_to_time(curr_pos)
