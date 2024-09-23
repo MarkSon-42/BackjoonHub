f4b21a2d231c7748e124fb99a9da0acca413475c
Minwoo Son <mark42son@gmail.com>
2024-09-23 14:48:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,19 @@
 from collections import deque
 
 def solution(maps):
-
-    n = len(maps)
-    m = len(maps[0])
-
-    dx = [0, 0, -1, 1]
-    dy = [1, -1, 0, 0]
-
+    n, m = len(maps), len(maps[0])
     q = deque()
     q.append((0, 0))
-
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
     while q:
-        x, y = q.popleft()
-
+        y, x = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1  # 여기가 핵심 라인. 거리 계산
-                q.append((nx, ny))
-
-    if maps[n - 1][m - 1] != 1:
-        return maps[n - 1][m - 1]
-    else:
-        return -1
-
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 1:
+                maps[ny][nx] = maps[y][x] + 1
+                q.append((ny, nx))
+    
+    return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1
